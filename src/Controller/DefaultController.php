2e4e4757d0f54c37dba7060e27a50002a439f1c1
zilo93 <ziloo78@gmail.com>
2019-07-22 21:10:32 +0200

    structure général + defaultController + first render

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/index", name="index")
+     */
     public function index()
     {
-
         return $this->render('default/index.html.twig');
+
     }
 }
